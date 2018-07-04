e1ce72fc6f6383cf654de895f7a9fb94cd3ae476
kevinhu253216 <kevinhu253216@gmail.com>
2018-07-04 16:10:19 -0400

    Initial commit

---
+++ src/TestThreadPriority.java
@@ -0,0 +1,30 @@
+import com.sun.xml.internal.bind.v2.model.annotation.RuntimeAnnotationReader;
+
+public class TestThreadPriority {
+
+    public static void main(String[] args){
+        Thread t1 = new Thread(new T1());
+        Thread t2 = new Thread(new T2());
+
+        t1.setPriority(Thread.NORM_PRIORITY + 3);//优先级高
+        t1.start();
+        t2.start();
+    }
+
+}
+
+class T1 implements Runnable{
+    public void run(){
+        for(int i=0;i<1000; i++){
+            System.out.println("T1:"+i);
+        }
+    }
+}
+
+class T2 implements Runnable{
+    public void run(){
+        for(int i=0;i<1000; i++){
+            System.out.println("T2:"+i);
+        }
+    }
+}
